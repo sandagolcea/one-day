c9d6100326c58742839250d1f83d62dcd82aa21a
Sanda Golcea <sanda.golcea@gmail.com>
2015-07-02 21:08:18 +0100

    Added basic database with professional model

---
+++ app/models/professionals.js
@@ -0,0 +1,17 @@
+var mongoose =  require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var professionalSchema = new Schema({
+  name: String
+});
+
+mongoose.model('Professional', professionalSchema);
+
+mongoose.connect('mongodb://localhost/one-day-test');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Successfully connected to mongodb..');
+});
--- server.js
+++ server.js
@@ -2,7 +2,21 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var mongoose = require('mongoose');
+
+require('./app/models/professionals.js');
+var Professional = mongoose.model('Professional');
+
 app.use('/', express.static(__dirname));
+
+app.get('/create', function(request, response) {
+  console.log('hi');
+  Professional.create({
+   name: 'Sanda'
+  }, function() {
+    response.send("Hello");
+  });
+});
 
 app.listen(port, function () {
   console.log('Listening on port '+port+'..');
